0323ef14f624c8ecdbfc57b9ca5b964ed12adc05
Patrick Mutua <pawatechsys@gmail.com>
2025-11-29 00:39:44 +0300

    create login tracker in the index file

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const createLoginTracker = function (
+  userInfo = {
+    username: "user1",
+    password: "password123",
+  }
+) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  return (passwordAttempt) => {
+    if (attemptCount >= maxAttempts) {
+      return "Account locked due to too many failed login attempts";
+    }
+    attemptCount++;
+    if (passwordAttempt === userInfo.password && attemptCount <= maxAttempts) {
+      return "Login successful";
+    }
 
-
+    if (attemptCount <= maxAttempts) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+};
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
